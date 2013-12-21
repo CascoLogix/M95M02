8eac4ba80eed3cdaa2f2b363ba78d49f7677b283
Microcas <devnull@localhost>
2013-12-21 09:10:39 -0600

    First commit

---
+++ M95M02.cpp
@@ -0,0 +1,35 @@
+
+
+
+#include <M95M02.h>
+
+
+
+void M95M02::begin()
+{
+
+}
+
+
+uint32_t M95M02Write(uint32_t address, uint8_t data)
+{
+
+}
+
+
+uint32_t M95M02Write(uint32_t address, uint8_t * buffer, uint32_t numBytes)
+{
+
+}
+
+
+uint32_t M95M02Read(uint32_t address, uint8_t data)
+{
+
+}
+
+
+uint32_t M95M02Read(uint32_t address, uint8_t * buffer, uint32_t numBytes)
+{
+
+}
---
+++ M95M02.h
@@ -0,0 +1,15 @@
+
+
+
+
+#include <M95M02.h>
+
+
+class M95M02 {
+	public:
+	
+	friend:
+	
+	private:
+
+}
